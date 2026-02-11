6b913db74fa57fc699614e1d8478b8ff0bd5809b
Game Fire <code@thegamefire.com>
2026-02-11 18:24:41 +0100

    Changed Slot Data to Be Shown in Tabs

--- src/routes/slot/[slotName]/+page.svelte
+++ src/routes/slot/[slotName]/+page.svelte
@@ -1,34 +1,51 @@
 <script lang="ts">
     import {onMount} from "svelte";
     import type {SlotTrackerData} from "$lib/types";
+    import {source} from "sveltekit-sse";
     import ReceivedItemTable from "$lib/components/ReceivedItemTable.svelte";
     import LocationTable from "$lib/components/LocationTable.svelte";
-    import {source} from "sveltekit-sse";
 
     let {data} = $props()
     const slotName = $derived(data.slotName);
 
     let tracker: SlotTrackerData = $state({
-        logs: [],
-        data: {game: "", collectedChecks: [], uncollectedChecks: [], receivedItems: [], deathCount: 0}
+    logs: [],
+    data: {game: "", collectedChecks: [], uncollectedChecks: [], receivedItems: [], deathCount: 0}
     });
 
     onMount(() => {
-        const trackerSource = source(`/api/slotdata/${slotName}`).select("message");
+    const trackerSource = source(`/api/slotdata/${slotName}`).select("message");
 
-        trackerSource.subscribe((message: string) => {
-            tracker = JSON.parse(message);
-        });
+    trackerSource.subscribe((message: string) => {
+        tracker = JSON.parse(message);
+    });
     })
 
-    $inspect(tracker);
+    let tabs = $derived([
+     /*{ name: "Console", comp: APConsole, props: {logs: } },*/
+     { name: "Received Items", comp: ReceivedItemTable, props: {items: tracker.data.receivedItems}  },
+     { name: "Locations", comp: LocationTable, props: {
+             collectedChecks: tracker.data.collectedChecks,
+             uncollectedChecks: tracker.data.uncollectedChecks
+         }  },
+    ]);
+
+    let selectedTabIndex = $state(0);
+    let selectedTab = $derived(tabs[selectedTabIndex]);
 </script>
-<div class="w-full flex flex-col items-center max-h-4/5">
-    <div class="w-6/7 md:w-4/5 rounded-xl mb-6 max-h-2/5 overflow-scroll">
-        <ReceivedItemTable items={tracker.data.receivedItems}/>
-    </div>
-    <div class="w-6/7 md:w-4/5 rounded-xl max-h-4/6 overflow-scroll mb-6">
-        <LocationTable collectedChecks={tracker.data.collectedChecks}
-                       uncollectedChecks={tracker.data.uncollectedChecks}/>
+
+
+<div class="flex flex-col items-center max-h-4/6">
+
+    <div class="w-6/7 md:w-4/5 max-h-full">
+        <div class="w-full mb-2">
+            {#each tabs as tab, i}
+                <button onclick={() => selectedTabIndex = i} class="mx-2 px-4 py-1 rounded-full {selectedTabIndex===i?'bg-violet-200':'active:bg-gray-200 hover:bg-gray-100'}">{tab.name}</button>
+            {/each}
+        </div>
+        <div class="rounded-xl overflow-scroll max-h-full">
+            <selectedTab.comp {...selectedTab.props} />
+        </div>
     </div>
 </div>
+
