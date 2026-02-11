e99f464b9def836ca983aed2659c4dcfdc3e74e5
Luniya De Wannemaeker <luniya.dewannemaeker@ugent.be>
2026-02-11 19:00:41 +0100

    fixed dumb mistake (wups)

--- src/routes/hints/+page.svelte
+++ src/routes/hints/+page.svelte
@@ -4,17 +4,29 @@
   import type {PublicHintRecieverMap} from '$lib/types';
   
   // fake data here \/, be sure to replace - Luniya
-  let hintMap = {
-  "Luniya" : {
-  name : "the thing",
-  sender : "the sender",
-  location : "the location",
-  },
+  let hints = [
+    {
+    reciever : "Luniya",
+    name : "the thing",
+    sender : "the sender",
+    location : "the location",
+    },
+    {
+    reciever : "meow",
+    name : "the thing",
+    sender : "the sender",
+    location : "the location",
+    } , {
+    reciever : "meow",
+    name : "the thing2",
+    sender : "the sender",
+    location : "the location2",
   }
+  ];
 </script>
 
 <div class="w-full flex flex-col items-center">
   <div class="w-6/7 md:w-4/5 rounded-xl overflow-hidden mb-6">
-      <HintTable hintMap={hintMap}/>
+      <HintTable hints={hints}/>
     </div>
 </div>
